88b96ae4ab462c6c5977a9af27db342d0e7beea1
phil <phil@flexsystem.com>
2018-05-16 17:35:34 +0800

    + add /_dimension/ base url

--- web/app/shared/services/media.service.ts
+++ web/app/shared/services/media.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from "@angular/core";
 export class MediaService {
     public getThumbnailUrl(mxc: string, width: number, height: number, method: "scale" | "crop" = "scale", isAnimated = true): string {
         mxc = mxc.substring("mxc://".length).split('?')[0];
-        return `/api/v1/dimension/media/thumbnail/${mxc}?width=${width}&height=${height}&method=${method}&animated=${isAnimated}`;
+        return `/_dimension/api/v1/dimension/media/thumbnail/${mxc}?width=${width}&height=${height}&method=${method}&animated=${isAnimated}`;
     }
 }
